09cbdd361638dd1ec017db32266fec0d0acb1163
Grezzt <andhika.prasetyo105@gmail.com>
2025-06-02 00:55:49 +0700

    dropdown tambah jabatan

--- petugas/tambah_karyawan.php
+++ petugas/tambah_karyawan.php
@@ -1,3 +1,7 @@
+<?php
+include '../koneksi.php';
+?>
+
 <div class="d-sm-flex align-items-center justify-content-between mb-4">
     <h1 class="h3 mb-0 text-gray-800">Tambah Karyawan</h1>
 </div>
@@ -10,14 +14,20 @@
             <input required type="text" name="no_hp" class="form-control mb-2" placeholder="Masukan No HP">
 
             <select required name="id_jabatan" class="form-control mb-2">
+                <option value="">-- Pilih Jabatan --</option>
                 <?php
-                    $pet = mysqli_query($koneksi, "SELECT*FROM jabatan");
-                    while ($data = mysqli_fetch_array($pet)) {
+                $query_jabatan = mysqli_query($koneksi, "SELECT id_jabatan, nama_jabatan FROM jabatan");
+                if ($query_jabatan && mysqli_num_rows($query_jabatan) > 0) {
+                    while ($data_jabatan = mysqli_fetch_assoc($query_jabatan)) {
                 ?>
-
-                <option value="<?php echo $data['id_jabatan']; ?>"><?php echo $data['nama_jabatan']; ?></option>
-
-
+                        <option value="<?php echo htmlspecialchars($data_jabatan['id_jabatan']); ?>">
+                            <?php echo htmlspecialchars($data_jabatan['nama_jabatan']); ?>
+                        </option>
+                    <?php
+                    }
+                } else {
+                    ?>
+                    <option value="">Tidak ada data jabatan</option>
                 <?php
                 }
                 ?>
